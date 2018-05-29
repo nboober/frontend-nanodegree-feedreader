5e2c3251408d1b7d42b1686a90e465af7a7b6867
Nick Boober <nboober@terpmail.umd.edu>
2018-05-29 10:55:23 -0400

    Wrote two test in RSS Feeds to loop through the RSS Feed and make sure the URL and names are defined. Created a new suite named The Menu.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,28 +31,49 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it("url defined", function(){
+           for (let feed = 0; feed < allFeeds.length; feed++){
+    			      let indFeed = allFeeds[feed];
+    		          let keys = Object.keys(indFeed);
+                  expect(keys[1]).toBe("url");
+                  let values = Object.values(indFeed);
+                  expect(values[1]).toBeDefined();
+                }
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+         it("name defined", function(){
+           for (let feed = 0; feed < allFeeds.length; feed++){
+               let indFeed = allFeeds[feed];
+                 let keys = Object.keys(indFeed);
+                  expect(keys[0]).toBe("name");
+                  let values = Object.values(indFeed);
+                  expect(values[0]).toBeDefined();
+                }
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe("The Menu", function(){
+      /* TODO: Write a test that ensures the menu element is
+       * hidden by default. You'll have to analyze the HTML and
+       * the CSS to determine how we're performing the
+       * hiding/showing of the menu element.
+       */
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+       /* TODO: Write a test that ensures the menu changes
+        * visibility when the menu icon is clicked. This test
+        * should have two expectations: does the menu display when
+        * clicked and does it hide when clicked again.
+        */
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
