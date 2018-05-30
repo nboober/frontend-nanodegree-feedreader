5b26944e360f561e3b8e6ccbcc1784041f885d10
Nick Boober <nboober@terpmail.umd.edu>
2018-05-30 19:50:14 -0400

    updated RSS Feeds url defined and RSS Feeds name defined tests so the name and URL keys can be placed in any order in the containers

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -35,9 +35,9 @@ $(function() {
            for (let feed = 0; feed < allFeeds.length; feed++){
     			      let indFeed = allFeeds[feed];
     		          let keys = Object.keys(indFeed);
-                  expect(keys[1]).toBe("url");
+                  expect(keys).toContain("url");
                   let values = Object.values(indFeed);
-                  expect(values[1]).toBeDefined();
+                  expect(values).toBeDefined();
                 }
          });
 
@@ -51,9 +51,9 @@ $(function() {
            for (let feed = 0; feed < allFeeds.length; feed++){
                let indFeed = allFeeds[feed];
                  let keys = Object.keys(indFeed);
-                  expect(keys[0]).toBe("name");
+                  expect(keys).toContain("name");
                   let values = Object.values(indFeed);
-                  expect(values[0]).toBeDefined();
+                  expect(values).toBeDefined();
                 }
          });
     });
@@ -91,21 +91,33 @@ $(function() {
     /* TODO: Write a new test suite named "Initial Entries" */
     describe("Initial Entries", function() {
 
+      beforeEach(function(done){
+        google.setOnLoadCallback(init);
+        done();
+
+      });
       /* TODO: Write a test that ensures when the loadFeed
        * function is called and completes its work, there is at least
        * a single .entry element within the .feed container.
        * Remember, loadFeed() is asynchronous so this test will require
        * the use of Jasmine's beforeEach and asynchronous done() function.
        */
+
+       it("is NOT empty", function(done){
+         expect(loadFeed.length).not.toBeLessThan(1);
+         done();
+       });
            });
+
 
   /* TODO: Write a new test suite named "New Feed Selection" */
   describe("New Feed Selection", function() {
-    
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
+
+     /* TODO: Write a test that ensures when a new feed is loaded
+      * by the loadFeed function that the content actually changes.
+      * Remember, loadFeed() is asynchronous.
+      */
+
   });
 
 
