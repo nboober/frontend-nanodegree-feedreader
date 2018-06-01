0bcb04f581cbdf335057d214469a612e66f9c21f
Nick Boober <nboober@terpmail.umd.edu>
2018-05-31 21:02:47 -0400

    created the beginning of new test, New Feed Selection is loaded. Incorporated the beforeEach function along with the begining of the it test function

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -113,11 +113,19 @@ $(function() {
   /* TODO: Write a new test suite named "New Feed Selection" */
   describe("New Feed Selection", function() {
 
+    beforeEach(function(done){
+      google.setOnLoadCallback(init);
+      done();
+
+    });
      /* TODO: Write a test that ensures when a new feed is loaded
       * by the loadFeed function that the content actually changes.
       * Remember, loadFeed() is asynchronous.
       */
+      it("is loaded", function(done){
 
+        done();
+      });
   });
 
 
